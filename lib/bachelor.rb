62f79b92bfbda01110fe121be8f24cac09ebb4cc
Charlotte Goldthwaite <chargold201@gmail.com>
2019-03-25 21:03:34 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -75,5 +75,5 @@ def get_average_age_for_season(data, season)
   end
   total = 0.0
   age_array.each {|age| total += age}
-  average = total / age_array.length
+  average = total / age_array.length.round
 end
